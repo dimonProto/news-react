f723a94c4c39fbf6e81af623911d0333c30dafc7
dimonProto <dimonvkedax@gmail.com>
2025-02-24 12:41:50 +0200

    add pagination, change api endpoint

--- src/assets/pages/Main/Main.jsx
+++ src/assets/pages/Main/Main.jsx
@@ -4,24 +4,45 @@ import { useEffect, useState } from "react";
 import { getNews } from "../../../api/apiNews";
 import NewsList from "../../../components/NewsList/NewsList";
 import Skeleton from "../../../components/Skeleton/Skeleton";
+import Pagination from "../../../components/Pagination/Pagination";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
+
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true);
+      const response = await getNews(currentPage, pageSize);
+      setNews(response.news);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true);
-        const response = await getNews();
-        setNews(response.news);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handlePages = (pageNum) => {
+    setCurrentPage(pageNum);
+  };
 
   return (
     <main className={styles.main}>
@@ -30,12 +51,25 @@ const Main = () => {
       ) : (
         <Skeleton type="banner" count={1} />
       )}
-
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePages={handlePages}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
       {!isLoading ? (
         <NewsList news={news} />
       ) : (
         <Skeleton type="item" count={10} />
       )}
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePages={handlePages}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
     </main>
   );
 };
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,37 @@
+import styles from "./styles.module.css";
+
+const Pagination = ({
+  totalPages,
+  handleNextPage,
+  handlePreviousPage,
+  handlePages,
+  currentPage,
+}) => {
+  return (
+    <div className={styles.pagination}>
+      <button onClick={handlePreviousPage} disabled={ currentPage <= 1} className={styles.arrow}>
+        {"<"}
+      </button>
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, index) => {
+          return (
+            <button
+              onClick={() => handlePages(index + 1)}
+              disabled={index + 1 === currentPage}
+              className={styles.pageNumber}
+              key={index}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+
+      <button onClick={handleNextPage} disabled={ currentPage >= totalPages} className={styles.arrow}>
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
