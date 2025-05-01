24468a196e2fb193fb2ea174f3da83e275d9af7c
dimonProto <dimonvkedax@gmail.com>
2025-05-01 11:11:16 +0300

    refactoring

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,79 @@
+import { getCategories, getNews } from "../../api/apiNews";
+import Categories from "../../components/Categories/Categories";
+import NewsBanner from "../../components/NewsBanner/NewsBanner";
+import NewsList from "../../components/NewsList/NewsList";
+import Pagination from "../../components/Pagination/Pagination";
+import Search from "../../components/Search/Search";
+import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constant";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { useFilters } from "../../helpers/hooks/useFilters";
+import styles from "./styles.module.css";
+
+const Main = () => {
+
+const {filters, changeFilter} = useFilters({
+  page_number: 1,
+  page_size: PAGE_SIZE,
+  category: null,
+  keywords: '',
+})
+  const debouncedKeywords = useDebounce(filters.keywords, 1500)
+
+  const {data, isLoading} = useFetch(getNews,{
+    ...filters,
+    keywords: debouncedKeywords,
+  })
+
+  const {data:dataCategories} = useFetch(getCategories)
+
+  const handleNextPage = () => {
+    if (filters.page_number < TOTAL_PAGES) {
+      changeFilter('page_number',filters.page_number + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (filters.page_number > 1) {
+      changeFilter('page_number',filters.page_number - 1);
+    }
+  };
+
+  const handlePages = (pageNum) => {
+    changeFilter('page_number', pageNum);
+  };
+
+
+  return (
+    <main className={styles.main}>
+     { dataCategories ? <Categories
+        categories={dataCategories.categories}
+        selectedCategory={filters.category}
+        setSelectedCategory={(category) => changeFilter('category', category) }
+      /> : null} 
+      <Search keywords={filters.keywords} setKeywords={(keywords) => changeFilter('keywords', keywords)}/>
+
+      <NewsBanner isLoading={isLoading} item={data && data.news && data.news[0]} /> 
+
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePages={handlePages}
+        totalPages={TOTAL_PAGES}
+        currentPage={filters.page_number}
+      />
+
+        <NewsList isLoading={isLoading} news={data?.news} /> 
+
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePages={handlePages}
+        totalPages={TOTAL_PAGES}
+        currentPage={filters.page_number}
+      />
+    </main>
+  );
+};
+
+export default Main;
