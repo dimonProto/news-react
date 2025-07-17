5d22bdc349dc55b220e09f8f60743fef9bf41515
dimonProto <dimonvkedax@gmail.com>
2025-07-17 14:37:01 +0300

    add context and change theme

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,17 @@
+import { useTheme } from "../../context/ThemeContext";
 import styles from "./styles.module.css";
 
 interface Props {
   keywords: string
   setKeywords: (keywords: string) => void
+
 }
 
 const Search = ({ keywords, setKeywords }: Props) => {
+
+   const {isDark} = useTheme()
   return (
-    <div  className={styles.search}>
+    <div className= {`${styles.search} ${isDark ? styles.dark : styles.light} `}>
       <input
         onChange={(e) => setKeywords(e.target.value)}
         type="text"
